24876e5d0e437b450ae81a089d12964d34238c1b
Joshua Villahermosa <joshvillahermosa@gmail.com>
2025-03-04 16:26:05 -0500

    feat(styles): add styles and configuration for headers, variables, and storybook integration

---
+++ src/styles/variables/font-weights.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import './index.scss';
+
+export default {
+  tags: ['styles', 'variables', 'autodocs'],
+  title: 'Styles/Font Weights',
+  parameters: {
+    docs: {
+      description: {
+        story: 'This story showcases the different font weights available in the design system.',
+      },
+    },
+  },
+} as Meta;
+
+const Template: StoryFn = () => (
+  <div>
+    <h1 className='header-1'>Font Weights</h1>
+    <p className="font-weight-extra-light">Extra Small Text - font-weight-extra-light</p>
+    <p className="font-weight-light">Small Text - font-weight-light</p>
+    <p className="font-weight-normal">Medium Text - font-weight-normal</p>
+    <p className="font-weight-bold">Large Text - font-weight-bold</p>
+    <p className="font-weight-extra-bold">Extra Large Text - font-weight-extra-bold</p>
+  </div>
+);
+
+export const Default = Template.bind({});
