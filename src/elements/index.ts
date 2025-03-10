7e000487ec7fdec05e7f611a70d694b9b3a2f555
Joshua Villahermosa <joshvillahermosa@gmail.com>
2025-03-10 18:30:11 -0400

    fix(icon): fix icon export

--- src/elements/index.ts
+++ src/elements/index.ts
@@ -1,11 +1,13 @@
 export { default as BackgroundImage } from './background-image/background-image';
 export { default as Container, defaultMargin } from './container/container';
 export { default as Heading } from './heading/heading';
+export { default as Icon } from './icon/icon';
 export { default as Image } from './image/image';
 export { default as Link } from './link/link';
 
 export type { BackgroundImageProps } from './background-image/background-image';
 export type { ContainerProps } from './container/container';
 export type { HeaderLevels, HeadingProps } from './heading/heading';
+export type { IconProps } from './icon/icon';
 export type { ImageProps } from './image/image';
 export type { LinkProps } from './link/link';
