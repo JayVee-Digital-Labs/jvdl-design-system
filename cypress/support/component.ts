efdba7ed0839c3b45b5177c39ace8dc3a33ea136
Joshua Villahermosa <joshvillahermosa@gmail.com>
2025-03-29 10:45:01 -0400

    chore(tests): add global css inject

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,4 +1,5 @@
 /// <reference types="@testing-library/cypress" />
+import '@/styles/globals.css';
 // ***********************************************************
 // This example support/component.ts is processed and
 // loaded automatically before your test files.
