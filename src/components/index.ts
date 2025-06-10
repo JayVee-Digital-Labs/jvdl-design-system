079fb0d8d2bac7b010c76310cbf3c13053b8f678
Joshua Villahermosa <joshvillahermosa@gmail.com>
2025-06-09 21:37:45 -0400

    fix(simple-nav-bar): export simple nav bar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 export { default as Avatar } from './avatar/avatar';
 export type { AvatarProps, AvatarSize } from './avatar/avatar';
+export { default as SimpleNavBar } from './simple-nav-bar/simple-nav-bar';
+export type { SimpleNavBarProps } from './simple-nav-bar/simple-nav-bar';
 export { default as SocialMediaBar } from './social-media-bar/social-media-bar';
 export type { SocialMediaBarProps } from './social-media-bar/social-media-bar';
 export { default as TechStack } from './tech-stack/tech-stack';
